9c87f49072fc3b9c4d22ea9d0aed74a5156da39a
ashishgoyal1018 <92246419+ashishgoyal1018@users.noreply.github.com>
2024-06-15 15:34:30 +0530

    Update Prediction_system_web_app.py

--- Prediction_system_web_app.py
+++ Prediction_system_web_app.py
@@ -24,18 +24,22 @@ except Exception as e:
 
 def trade_prediction(input_data):
 # changing the input_data to numpy array
-   input_data_as_numpy_array = np.asarray(input_data)
+    try:
+     input_data_as_numpy_array = np.asarray(input_data)
 
 # reshape the array as we are predicting for one instance
-   input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-   prediction = loaded_model.predict(input_data_reshaped)
-   print(prediction)
+       prediction = loaded_model.predict(input_data_reshaped)
+       print(prediction)
 
-   if (prediction[0] == 0):
-        return'The trade has more than 85% chances of hitting the target'
-   else:
-        return'The trade has more than 85% chances of hitting the stoploss'
+       if (prediction[0] == 0):
+            return'The trade has more than 85% chances of hitting the target'
+       else:
+            return'The trade has more than 85% chances of hitting the stoploss'
+    except Exception as e:
+        st.error(f"Error during prediction: {e}")
+        return None
   
 def main():
     st.title('Day Trade Prediction ML Model')
