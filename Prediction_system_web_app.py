8e7d8cee9e767f94aa96389db8211301baed5602
ashishgoyal1018 <92246419+ashishgoyal1018@users.noreply.github.com>
2024-06-17 15:31:41 +0530

    Update Prediction_system_web_app.py

--- Prediction_system_web_app.py
+++ Prediction_system_web_app.py
@@ -44,7 +44,7 @@ def trade_prediction(input_data):
         return None
 
 def main():
-    st.title('Day Trade Prediction ML Model')
+    st.title('Chart Pattern Profitability Prediction')
     
     Date_2= st.number_input('Enter Date of trade',min_value=1,max_value=31,value=15)
     
@@ -54,25 +54,25 @@ def main():
     Day_encoded= Day_encoded_[Day]
 
     dropdown_options_2= ["BankNifty", "Nifty"]
-    Symbol= st.selectbox("Select the Symbol:", dropdown_options_2)
+    Symbol= st.selectbox("Select the Index:", dropdown_options_2)
     Symbol_encoded_= {"BankNifty":0, "Nifty":1}
     Symbol_encoded= Symbol_encoded_[Symbol]
     
-    dropdown_options_3= ["3", "5","10","15"]
-    Time_Frame= st.selectbox("Select the timeframe:", dropdown_options_3)
-    Time_Frame_encoded_= {"15":0, "10":0.33,"5":0.66,"3":1}
+    dropdown_options_3= ["3min", "5min","10min","15min"]
+    Time_Frame= st.selectbox("Select the Timeframe:", dropdown_options_3)
+    Time_Frame_encoded_= {"15min":0, "10min":0.33,"5min":0.66,"3min":1}
     Time_Frame_encoded= Time_Frame_encoded_[Time_Frame]
     
     dropdown_options_4= ["9","10","11","12","13","14","15"]
     Time_of_BC_candle_hr_2= st.selectbox("Time of BC candle: Hour:", dropdown_options_4)
-    Time_of_BC_candle_min_2= st.number_input('Time of BC candle": Minutes',min_value=0,max_value=59,value=30)
+    Time_of_BC_candle_min_2= st.number_input("Time of BC candle: Minutes",min_value=0,max_value=59,value=30)
 
     dropdown_options_5= ["Yes", "No"]
-    Distribution_0_yes_1_no= st.selectbox("Whether chart pattern as per image", dropdown_options_5)
+    Distribution_0_yes_1_no= st.selectbox("Whether setup as per Chart pattern", dropdown_options_5)
     Distribution_0_yes_1_no_encoded_= {"Yes":0, "No":1}
     Distribution_0_yes_1_no_encoded= Distribution_0_yes_1_no_encoded_[Distribution_0_yes_1_no]
 
-    India_Vix_2= st.number_input('Enter the india vix at time of trade',min_value=13,max_value=26,value=19)
+    India_Vix_2= st.number_input('Enter the India Vix at time of trade',min_value=13,max_value=26,value=19)
     
     dropdown_options_6= ["Bullish", "Bearish","Neutral"]
     Trend_Day_Chart_1_Bullish_0_Bearish= st.selectbox("Trend in Day chart", dropdown_options_6)
@@ -85,12 +85,12 @@ def main():
     Trend_1hr_Chart_1_Bullish_0_Bearish_encoded= Trend_Day_Chart_1_Bullish_0_Bearish_encoded_[Trend_1hr_Chart_1_Bullish_0_Bearish]
 
     dropdown_options_8= ["Gap Up", "Gap Down","Flat"]
-    Gap_up_0_down_1= st.selectbox("If Gap up Select 0, for gap down 1", dropdown_options_8)
+    Gap_up_0_down_1= st.selectbox("Select the Index Opening", dropdown_options_8)
     Gap_up_0_down_1_encoded_= {"Gap Up":1, "Gap Down":0, "Flat":0.5}
     Gap_up_0_down_1_encoded= Gap_up_0_down_1_encoded_[Gap_up_0_down_1]
 
     dropdown_options_9= ["Yes", "No"]
-    Twice_high_volume_0_Yes_1_No= st.selectbox("BC volume double the bearish candle volume select 0, else 1", dropdown_options_9)
+    Twice_high_volume_0_Yes_1_No= st.selectbox("Is the BC volume double the next candle volume", dropdown_options_9)
     Twice_high_volume_0_Yes_1_No_encoded_= {"Yes":0, "No":1}
     Twice_high_volume_0_Yes_1_No_encoded= Twice_high_volume_0_Yes_1_No_encoded_[Twice_high_volume_0_Yes_1_No]
 
